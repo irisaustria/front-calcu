4803d84f977e2c71f62aa0bd2790d61e3a20526a
irisaustria <iriscristiaustria@gmail.com>
2026-03-30 07:26:04 -0600

    solver-linear.js

--- js/solver-linear.js
+++ js/solver-linear.js
@@ -2,7 +2,7 @@ window.solveLinear = async function () {
   const P = document.getElementById("lin-P").value.trim();
   const Q = document.getElementById("lin-Q").value.trim();
 
-  const response = await fetch("http://127.0.0.1:5000/solve", {
+  const response = await fetch("https://back-calcu-but6.onrender.com/solve", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
