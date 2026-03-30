1f0cdc5110c6e9b1cd8fb46c1f32cc513c80dfb9
irisaustria <iriscristiaustria@gmail.com>
2026-03-30 07:25:29 -0600

    solver-integrating.js

--- js/solver-integrating.js
+++ js/solver-integrating.js
@@ -2,7 +2,7 @@ window.solveIntegrating = async function () {
   const M = document.getElementById("int-M").value.trim();
   const N = document.getElementById("int-N").value.trim();
 
-  const response = await fetch("https://back-calcu-but6.onrender.com", {
+  const response = await fetch("https://back-calcu-but6.onrender.com/solve", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
